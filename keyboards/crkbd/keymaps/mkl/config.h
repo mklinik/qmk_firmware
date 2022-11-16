e3514d4b7f9018d8a53e6aad9c0823097582f518
Markus Klinik <markus@chordify.net>
2022-11-16 07:29:50 +0100

    enable caps word

--- keyboards/crkbd/keymaps/mkl/config.h
+++ keyboards/crkbd/keymaps/mkl/config.h
@@ -33,6 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
--- keyboards/crkbd/keymaps/mkl/rules.mk
+++ keyboards/crkbd/keymaps/mkl/rules.mk
@@ -3,3 +3,4 @@ RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
+CAPS_WORD_ENABLE = yes
