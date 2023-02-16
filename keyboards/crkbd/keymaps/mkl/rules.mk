ad6a695145ada8df3a27fa02453096bd2441a126
Markus Klinik <mkl@lambdanaut.net>
2023-02-16 11:25:51 +0100

    enable debounce

--- keyboards/crkbd/keymaps/mkl/rules.mk
+++ keyboards/crkbd/keymaps/mkl/rules.mk
@@ -2,5 +2,9 @@ MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
+# link-time optimization shrinks the image
 LTO_ENABLE      = yes
 CAPS_WORD_ENABLE = yes
+
+# I have trouble with the n-key, let's try this
+DEBOUNCE_TYPE = sym_defer_g
