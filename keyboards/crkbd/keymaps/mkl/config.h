f16b33b39703096f8d97358f2a0ad9f566b10b43
Markus Klinik <mkl@lambdanaut.net>
2023-02-16 11:26:02 +0100

    increase tapping term

--- keyboards/crkbd/keymaps/mkl/config.h
+++ keyboards/crkbd/keymaps/mkl/config.h
@@ -26,10 +26,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define MASTER_LEFT
 #define MASTER_RIGHT
+// take handedness from an EEPROM setting
 // #define EE_HANDS
+// someone said this might help with boot hang problem, but for some reason it swaps sides, the left
+// side is always the right side and vice versa
+//#define SPLIT_USB_DETECT
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 250
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
