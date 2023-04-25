718131647cba7561d41ef2462cc629e327c80e2f
Markus Klinik <mkl@lambdanaut.net>
2023-04-25 10:23:45 +0200

    better flash script

--- flash.sh
+++ flash.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-sleep 5; pmount sda1; cp crkbd_rev1_mkl_promicro_rp2040.uf2 /media/sda1; sync; pumount sda1
+DEVICE=$1
+
+if [ -z $DEVICE ]
+then
+  echo "usage: $0 <device>"
+  echo "<device> as what pmount expects, for example sda1"
+  exit 1
+fi
+
+sleep 5
+pmount $DEVICE
+cp crkbd_rev1_mkl_promicro_rp2040.uf2 /media/$DEVICE
+sync
+pumount $DEVICE
